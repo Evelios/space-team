a1d9786253e9cdcffd2e3b1b7c890e721d23093f
Thomas Waters <twaters117@gmail.com>
2022-12-06 15:30:06 -0500

    Button debouncing

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from display.lcd import Lcd
+from sensor.button import Button
 
 lcd = Lcd(rs=16, e=17, d4=18, d5=19, d6=20, d7=21)
 lcd.display(on=True, cursor=True, blink=True)
@@ -8,3 +9,8 @@ lcd.write('Hi, Human.')
 
 lcd.move_to(1, 1)
 lcd.write('Hello, Pico!')
+
+button = Button(2)
+
+while True:
+    pass
