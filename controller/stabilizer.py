998ee216a9f30674e14e74b28e8cce03e6065846
Thomas Waters <twaters117@gmail.com>
2023-08-02 15:54:37 -0400

    Adding support for stabilizer

--- controller/stabilizer.py
+++ controller/stabilizer.py
@@ -14,6 +14,7 @@ class Stabilizer:
 
     rotary_encoder = RotaryEncoder
     """stabilizer_input"""
+    previous_reading = 0
 
     def __init__(self, p1, p2, p3, p4):
         self.rotary_encoder = RotaryEncoder(p1, p2, p3, p4)
@@ -22,11 +23,18 @@ class Stabilizer:
         self.lcd = I2cLcd(i2c=i2c, i2c_addr=0x3f, num_lines=2, num_columns=16)
 
     def update(self):
-        self.stability = self.rotary_encoder.read()
+        encoder_reading = self.rotary_encoder.read()
+
+        if encoder_reading == self.previous_reading:
+            return
+
         self.lcd.move_to(0, 0)
-        self.lcd.putstr("  ")
+        self.lcd.putstr("     ")
         self.lcd.move_to(0, 0)
-        self.lcd.putstr(str(self.stability))
+        # self.lcd.putstr(f"{encoder_reading} : {self.stability}")
         self.lcd.move_to(0, 1)
-        self.lcd.putstr(f"{self.rotary_encoder.v1} {self.rotary_encoder.v2} {self.rotary_encoder.v3} {self.rotary_encoder.v4}")
+        # self.lcd.putstr(f"{self.rotary_encoder.v1} {self.rotary_encoder.v2} {self.rotary_encoder.v3} {self.rotary_encoder.v4}")
+
+        self.stability += self.previous_reading - encoder_reading
+        self.previous_reading = encoder_reading
 
--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import time
-
 from controller.stabilizer import Stabilizer
 
 stabilizer = Stabilizer(3,2, 4, 5)
 
 while (True):
     stabilizer.update()
-    time.sleep_ms(100)
